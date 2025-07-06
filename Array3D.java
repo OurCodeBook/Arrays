5aaa685098408ea563637d2f9494f42506445ffb
MyCodeBook <pinkblossom548@gmail.com>
2025-07-06 21:20:21 +0530

    Array3D.java

--- Array3D.java
+++
@@ -1,8 +0,0 @@
-public class Array3D {
-    public static void main(String[] args) {
-        int[][][] arr = new int[4][5][6];
-        System.out.println(arr.length);
-        System.out.println(arr[0].length);
-        System.out.println(arr[0][0].length);
-    }   
-}
