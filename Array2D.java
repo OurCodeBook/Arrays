9a151c1a4eafd0b4dc861fd4bd68f8d7bfab8394
MyCodeBook <pinkblossom548@gmail.com>
2025-07-06 20:44:48 +0530

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,7 @@
+public class Array2D {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][7];
+        System.out.println("Row Count : "+arr.length);
+        System.out.println("Column Count : "+arr[0].length);
+    }
+}
